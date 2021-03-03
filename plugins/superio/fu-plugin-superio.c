tari/fwupd

commit 79a217a2a617224465e02c8cd7ebb8d3c5277c25

superio: Use DeviceInstanceIDs to merge chip quirk metadata
This means we can use the standard ->set_quirk_kv() vfunc rather than using
FuQuirks directly with a custom group.

plugins/superio/fu-plugin-superio.c
@@ -19,52 +19,29 @@ fu_plugin_superio_coldplug_chipset (FuPlugin *plugin, const gchar *chipset, GErr
 	const gchar *dmi_vendor;
 	g_autoptr(FuSuperioDevice) dev = NULL;
 	g_autoptr(FuDeviceLocker) locker = NULL;
-	g_autofree gchar *key = g_strdup_printf ("SuperIO=%s", chipset);
-	guint64 id;
-	guint64 port;
-
-	/* get ID we need for the chipset */
-	id = fu_plugin_lookup_quirk_by_id_as_uint64 (plugin, key, "Id");
-	if (id == 0x0000 || id > 0xffff) {
-		g_set_error (error,
-			     G_IO_ERROR,
-			     G_IO_ERROR_NOT_SUPPORTED,
-			     "SuperIO chip %s has invalid Id", chipset);
-		return FALSE;
-	}
-
-	/* set address */
-	port = fu_plugin_lookup_quirk_by_id_as_uint64 (plugin, key, "Port");
-	if (port == 0x0 || port > 0xffff) {
-		g_set_error (error,
-			     G_IO_ERROR,
-			     G_IO_ERROR_NOT_SUPPORTED,
-			     "SuperIO chip %s has invalid Port", chipset);
-		return FALSE;
-	}
 
 	/* create IT89xx or IT89xx */
-	if (id >> 8 == 0x85) {
+	if (g_strcmp0 (chipset, "IT8587") == 0) {
 		dev = g_object_new (FU_TYPE_SUPERIO_IT85_DEVICE,
 				    "device-file", "/dev/port",
 				    "chipset", chipset,
-				    "id", id,
-				    "port", port,
 				    NULL);
-	} else if (id >> 8 == 0x89) {
+	} else if (g_strcmp0 (chipset, "IT8987") == 0) {
 		dev = g_object_new (FU_TYPE_SUPERIO_IT89_DEVICE,
 				    "device-file", "/dev/port",
 				    "chipset", chipset,
-				    "id", id,
-				    "port", port,
 				    NULL);
 	} else {
 		g_set_error (error,
 			     G_IO_ERROR,
 			     G_IO_ERROR_NOT_SUPPORTED,
-			     "SuperIO chip %s has unsupported Id", chipset);
+			     "SuperIO chip %s has unsupported SuperioId", chipset);
 		return FALSE;
 	}
+
+	/* set ID and port via quirks */
+	if (!fu_device_probe (FU_DEVICE (dev), error))
+		return FALSE;
 
 	/* set vendor ID as the motherboard vendor */
 	dmi_vendor = fu_plugin_get_dmi_value (plugin, FU_HWIDS_KEY_BASEBOARD_MANUFACTURER);
@@ -79,17 +56,6 @@ fu_plugin_superio_coldplug_chipset (FuPlugin *plugin, const gchar *chipset, GErr
 		return FALSE;
 	fu_plugin_device_add (plugin, FU_DEVICE (dev));
 
-	return TRUE;
-}
-
-static gboolean
-fu_plugin_superio_coldplug_chipsets (FuPlugin *plugin, const gchar *str, GError **error)
-{
-	g_auto(GStrv) chipsets = g_strsplit (str, ",", -1);
-	for (guint i = 0; chipsets[i] != NULL; i++) {
-		if (!fu_plugin_superio_coldplug_chipset (plugin, chipsets[i], error))
-			return FALSE;
-	}
 	return TRUE;
 }
 
@@ -121,7 +87,7 @@ fu_plugin_coldplug (FuPlugin *plugin, GError **error)
 		tmp = fu_plugin_lookup_quirk_by_id (plugin, key, FU_QUIRKS_SUPERIO_CHIPSETS);
 		if (tmp == NULL)
 			continue;
-		if (!fu_plugin_superio_coldplug_chipsets (plugin, tmp, error))
+		if (!fu_plugin_superio_coldplug_chipset (plugin, tmp, error))
 			return FALSE;
 	}
 	return TRUE;

plugins/superio/fu-superio-device.c
@@ -27,8 +27,6 @@ G_DEFINE_TYPE_WITH_PRIVATE (FuSuperioDevice, fu_superio_device, FU_TYPE_UDEV_DEV
 enum {
 	PROP_0,
 	PROP_CHIPSET,
-	PROP_PORT,
-	PROP_ID,
 	PROP_LAST
 };
 
@@ -131,6 +129,15 @@ fu_superio_device_check_id (FuSuperioDevice *self, GError **error)
 {
 	FuSuperioDevicePrivate *priv = GET_PRIVATE (self);
 	guint16 id_tmp;
+
+	/* no quirk entry */
+	if (priv->id == 0x0) {
+		g_set_error_literal (error,
+				     G_IO_ERROR,
+				     G_IO_ERROR_NOT_SUPPORTED,
+				     "invalid SuperioId");
+		return FALSE;
+	}
 
 	/* check ID, which can be done from any LDN */
 	if (!fu_superio_device_regval16 (self, SIO_LDNxx_IDX_CHIPID1, &id_tmp, error))
@@ -354,12 +361,6 @@ fu_superio_device_get_property (GObject *object, guint prop_id,
 	case PROP_CHIPSET:
 		g_value_set_string (value, priv->chipset);
 		break;
-	case PROP_PORT:
-		g_value_set_uint (value, priv->port);
-		break;
-	case PROP_ID:
-		g_value_set_uint (value, priv->id);
-		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 		break;
@@ -377,16 +378,52 @@ fu_superio_device_set_property (GObject *object, guint prop_id,
 		g_free (priv->chipset);
 		priv->chipset = g_value_dup_string (value);
 		break;
-	case PROP_PORT:
-		priv->port = g_value_get_uint (value);
-		break;
-	case PROP_ID:
-		priv->id = g_value_get_uint (value);
-		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 		break;
 	}
+}
+
+static gboolean
+fu_superio_device_set_quirk_kv (FuDevice *device,
+				const gchar *key,
+				const gchar *value,
+				GError **error)
+{
+	FuSuperioDevice *self = FU_SUPERIO_DEVICE (device);
+	FuSuperioDevicePrivate *priv = GET_PRIVATE (self);
+
+	if (g_strcmp0 (key, "SuperioId") == 0) {
+		guint64 tmp = fu_common_strtoull (value);
+		if (tmp < G_MAXUINT16) {
+			priv->id = tmp;
+			return TRUE;
+		}
+		g_set_error_literal (error,
+				     G_IO_ERROR,
+				     G_IO_ERROR_INVALID_DATA,
+				     "invalid value");
+		return FALSE;
+	}
+	if (g_strcmp0 (key, "SuperioPort") == 0) {
+		guint64 tmp = fu_common_strtoull (value);
+		if (tmp < G_MAXUINT16) {
+			priv->port = tmp;
+			return TRUE;
+		}
+		g_set_error_literal (error,
+				     G_IO_ERROR,
+				     G_IO_ERROR_INVALID_DATA,
+				     "invalid value");
+		return FALSE;
+	}
+
+	/* failed */
+	g_set_error_literal (error,
+			     G_IO_ERROR,
+			     G_IO_ERROR_NOT_SUPPORTED,
+			     "quirk key not supported");
+	return FALSE;
 }
 
 static void
@@ -421,21 +458,10 @@ fu_superio_device_class_init (FuSuperioDeviceClass *klass)
 				     G_PARAM_CONSTRUCT_ONLY |
 				     G_PARAM_STATIC_NAME);
 	g_object_class_install_property (object_class, PROP_CHIPSET, pspec);
-	pspec = g_param_spec_uint ("port", NULL, NULL,
-				   0, G_MAXUINT, 0,
-				   G_PARAM_READWRITE |
-				   G_PARAM_CONSTRUCT_ONLY |
-				   G_PARAM_STATIC_NAME);
-	g_object_class_install_property (object_class, PROP_PORT, pspec);
-	pspec = g_param_spec_uint ("id", NULL, NULL,
-				   0, G_MAXUINT, 0,
-				   G_PARAM_READWRITE |
-				   G_PARAM_CONSTRUCT_ONLY |
-				   G_PARAM_STATIC_NAME);
-	g_object_class_install_property (object_class, PROP_ID, pspec);
 
 	object_class->finalize = fu_superio_device_finalize;
 	klass_device->to_string = fu_superio_device_to_string;
+	klass_device->set_quirk_kv = fu_superio_device_set_quirk_kv;
 	klass_device->probe = fu_superio_device_probe;
 	klass_device->setup = fu_superio_device_setup;
 	klass_device->prepare_firmware = fu_superio_device_prepare_firmware;

plugins/superio/superio.quirk
@@ -26,34 +26,34 @@ InstallDuration=20
 SuperioChipsets=IT8987
 InstallDuration=20
 
-[SuperIO=IT8510]
-Id=0x8510
-Port=0x2e
+[DeviceInstanceId=SuperIO-IT8510]
+SuperioId=0x8510
+SuperioPort=0x2e
 
-[SuperIO=IT8511]
-Id=0x8511
-Port=0x2e
+[DeviceInstanceId=SuperIO-IT8511]
+SuperioId=0x8511
+SuperioPort=0x2e
 
-[SuperIO=IT8512]
-Id=0x8512
-Port=0x2e
+[DeviceInstanceId=SuperIO-IT8512]
+SuperioId=0x8512
+SuperioPort=0x2e
 
-[SuperIO=IT8513]
-Id=0x8513
-Port=0x2e
+[DeviceInstanceId=SuperIO-IT8513]
+SuperioId=0x8513
+SuperioPort=0x2e
 
-[SuperIO=IT8516]
-Id=0x8516
-Port=0x2e
+[DeviceInstanceId=SuperIO-IT8516]
+SuperioId=0x8516
+SuperioPort=0x2e
 
-[SuperIO=IT8518]
-Id=0x8518
-Port=0x2e
+[DeviceInstanceId=SuperIO-IT8518]
+SuperioId=0x8518
+SuperioPort=0x2e
 
-[SuperIO=IT8587]
-Id=0x8587
-Port=0x2e
+[DeviceInstanceId=SuperIO-IT8587]
+SuperioId=0x8587
+SuperioPort=0x2e
 
-[SuperIO=IT8987]
-Id=0x8987
-Port=0x4e
+[DeviceInstanceId=SuperIO-IT8987]
+SuperioId=0x8987
+SuperioPort=0x4e

commit fb067eccb3e382449340b17d8613d6835e9806e5

superio: Ensure quirks are set on the new devices
Fixes a regression in 79a217a2a617224465e02c8cd7ebb8d3c5277c25

plugins/superio/fu-plugin-superio.c
@@ -25,11 +25,13 @@ fu_plugin_superio_coldplug_chipset (FuPlugin *plugin, const gchar *chipset, GErr
 		dev = g_object_new (FU_TYPE_SUPERIO_IT85_DEVICE,
 				    "device-file", "/dev/port",
 				    "chipset", chipset,
+				    "quirks", fu_plugin_get_quirks (plugin),
 				    NULL);
 	} else if (g_strcmp0 (chipset, "IT8987") == 0) {
 		dev = g_object_new (FU_TYPE_SUPERIO_IT89_DEVICE,
 				    "device-file", "/dev/port",
 				    "chipset", chipset,
+				    "quirks", fu_plugin_get_quirks (plugin),
 				    NULL);
 	} else {
 		g_set_error (error,